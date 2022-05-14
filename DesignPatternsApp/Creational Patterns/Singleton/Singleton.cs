02496a494f1fefc8f8226d618d3cee4e249080b5
EKamilOrlowski <e.kamil.orlowski@gmail.com>
2022-05-14 17:45:11 +0200

    Add project files.

---
+++ DesignPatternsApp/Creational Patterns/Singleton/Singleton.cs
@@ -0,0 +1,30 @@
+﻿namespace DesignPatternsApp.Creational_Patterns.Singleton
+{
+    public class Singleton
+    {
+        private Singleton() { }
+
+        private static readonly object _lock = new object();
+        private static Singleton _instance;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+                return _instance;
+            }
+            private set { }
+        }
+
+        public void RunBusinessLogic()
+        {
+        }
+    }
+}
